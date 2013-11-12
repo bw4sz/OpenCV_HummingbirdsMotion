6305222d8d12700990714c70780a64d3848f78f3
bw4sz <benweinstein2010@gmail.com>
2013-11-12 09:16:06 -0500

    added test

--- HumMotion.py
+++ HumMotion.py
@@ -3,8 +3,8 @@ import numpy as np
 
 
 # Load an color image in grayscale
-img = cv2.imread('C:/Users/Jorge/Dropbox/Thesis/Maquipucuna_SantaLucia/FlowerPhotos/17.jpg',cv2.CV_LOAD_IMAGE_COLOR)
+img = cv2.imread('C:/Users/Ben/Dropbox/Thesis/Maquipucuna_SantaLucia/FlowerPhotos/17.jpg',cv2.CV_LOAD_IMAGE_COLOR)
 
+cv2.namedWindow("image", 1)
 cv2.imshow('image',img)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+cv2.waitKey(10)
