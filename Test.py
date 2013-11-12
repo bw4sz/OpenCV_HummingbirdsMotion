067b3d784fe23cbd5a25bba0cf181f0e3c0bd531
bw4sz <benweinstein2010@gmail.com>
2013-11-12 13:36:58 -0500

    same day

--- Test.py
+++ Test.py
@@ -1,11 +1,26 @@
 import cv2
-import numpy
+#import cv2.cv as cv
+import numpy as np
 
-photo = 'C:/Users/Ben/Dropbox/Posters_Presentations/PredObs_AMNH/Images/Gorgeted-Sunangel-tbl_4172'
-
+#Begin with a still image test to show that package works.
+photo = 'C:/Users/Ben/Documents/OpenCV_HummingbirdsMotion/17.jpg'
 
 # Load an color image in grayscale
-img = cv2.imread(photo,0)
+#img = cv2.imread(photo)
+#cv2.NamedWindow('my window', cv.CV_WINDOW_AUTOSIZE)
+#cv2.imshow('my window',img)
+#cv2.waitKey(0)
+#cv2.destroyWindow('my window')
 
-cv2.imshow("Test",img)
+#Try a sample HD video
+#Define Input file
+#Sample file from online
+cap = cv2.VideoCapture("C:/Users/Ben/Dropbox/Thesis/Maquipucuna_SantaLucia/openCV/MVI_0572.AVI")
+ret, frame = cap.read()
+
+print(frame)
+#show first image
+cv2.imshow('frame',frame)
+cv2.waitKey(100)
+cv2.destroyWindow('frame')
 
