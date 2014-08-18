15b0c6552f7ead054eca645ce4b3d494adceca13
bw4sz <benweinstein2010@gmail.com>
2014-08-18 13:17:36 -0400

    Version 1.5 installer, needed to add geos.dll to py2exe and place the data files directly in dist so path is set

--- Setup.py
+++ Setup.py
@@ -6,7 +6,10 @@ sys.argv.append('py2exe')
 
 setup(
     console=['motion_dev.py'],
-    data_files=[('data', ['testing/PlotwatcherTest.TLV',"Bisque_DEV/public/thumbnail.ico","C:\Python27\opencv_ffmpeg249.dll"])],
-    options = {"py2exe":{"dll_excludes": ["MSVCP90.dll"]}}    
+    data_files=[('', ['testing/PlotwatcherTest.TLV',"Bisque_DEV/public/thumbnail.ico","C:\Python27\opencv_ffmpeg249.dll","C:\Python27\Lib\site-packages\shapely\DLLs\geos.dll"])],
+    options = {"py2exe":{
+        'packages':['shapely'],
+        "dll_excludes": ["MSVCP90.dll"]
+    }}    
 )
 
