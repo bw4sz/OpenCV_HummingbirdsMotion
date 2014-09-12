14dde56a6bf035cb78c3b299ab18adf7c582100f
bw4sz <benweinstein2010@gmail.com>
2014-09-12 09:16:45 -0400

    ADDED new background subtractor test and profiler for KNN and mixture of gaussians

--- testing/MOG.py
+++ testing/MOG.py
@@ -1,19 +1,29 @@
 import numpy as np
 import cv2
 
-cap = cv2.VideoCapture('C:/Users/Jorge/Documents/OpenCV_HummingbirdsMotion/testing/PlotwatcherTest.tlv')
+cap = cv2.VideoCapture('C:/Users/Ben/Downloads/exampleMB.mp4')
 
-fgbg = cv2.BackgroundSubtractorMOG(500,.6,.9,.1)
+fgbgMOG2 = cv2.createBackgroundSubtractorMOG2()
+fgbgKNN = cv2.createBackgroundSubtractorMOG2()
 
 while(1):
     ret, frame = cap.read()
 
-    fgmask = fgbg.apply(frame)
+    fgmask = fgbgMOG2.apply(frame)
+    fgmaskKNN = fgbgKNN.apply(frame)
 
+    cv2.namedWindow('frame', cv2.WINDOW_NORMAL)
     cv2.imshow('frame',fgmask)
     k = cv2.waitKey(30) & 0xff
     if k == 27:
         break
+    
+    cv2.namedWindow('frameKNN', cv2.WINDOW_NORMAL)    
+    cv2.imshow('frameKNN',fgmaskKNN)
+    k = cv2.waitKey(30) & 0xff
+    if k == 27:
+        break
+    
 
 cap.release()
 cv2.destroyAllWindows()
