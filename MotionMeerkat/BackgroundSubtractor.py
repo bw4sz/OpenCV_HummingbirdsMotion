800c1c55fc2990539457cd145c1748e76ab66970
bw4sz <benweinstein2010@gmail.com>
2014-09-22 17:08:14 -0400

    MOG history added, ROI writing streamlined, needs to be checked

--- MotionMeerkat/BackgroundSubtractor.py
+++ MotionMeerkat/BackgroundSubtractor.py
@@ -4,23 +4,20 @@ import cv2
 #takes in the constructor and the method chosen
 
 class Background:
-    def __init__(self,subMethod,display_image,acc,thresh):
+    def __init__(self,subMethod,display_image,acc,thresh,moghistory):
     
         ##Subtractor Method
         self.subMethod=subMethod
         
         ####Create Background Constructor
         if self.subMethod=="Acc":
-                self.grey_image = np.uint8(display_image)
                 self.running_average_image = np.float32(display_image)
-                self.running_average_in_display_color_depth = display_image.copy()
-                self.difference =  display_image.copy()
                 self.accAvg=acc
                 self.threshT=thresh
     
         if self.subMethod=="MOG":
             #MOG method creator
-            self.fgbg = cv2.createBackgroundSubtractorMOG2()
+            self.fgbg = cv2.createBackgroundSubtractorMOG2(history=moghistory, detectShadows=False)
         if self.subMethod=="KNN":
             #MOG method creator
             self.fgbg = cv2.createBackgroundSubtractorKNN()
@@ -54,9 +51,7 @@ class Background:
             
             # Threshold the image to a black and white motion mask:
             ret,self.grey_image = cv2.threshold(self.grey_image, self.threshT, 255, cv2.THRESH_BINARY )
-        
-        #display("Before closing",1500,grey_image)
-        
+                
         ##Mixture of Gaussians
         if self.subMethod in ["MOG","KNN"]:
             self.grey_image = self.fgbg.apply(camera_imageROI)
