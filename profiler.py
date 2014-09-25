a68deaa7448f8c404b4b56a543d737b38aefe081
bw4sz <benweinstein2010@gmail.com>
2014-09-25 14:22:21 -0400

    Fixed exclusion box and added swinnen comparison check

--- profiler.py
+++ profiler.py
@@ -2,7 +2,7 @@ import pstats
 import os
 import cProfile
 
-os.system("python -m cProfile -o C:/MotionMeerkat/profile.txt C:/Users/Ben/Documents/OpenCV_HummingbirdsMotion/MotionMeerkat/main.py  --set_ROI --ROI=exclude --adapt --scan=1")
+os.system("python -m cProfile -o C:/MotionMeerkat/profile.txt C:/Users/Ben/Documents/OpenCV_HummingbirdsMotion/MotionMeerkat/main.py  --set_ROI --ROI=exclude --subMethod=MOG --inDEST="C:\Users\Ben\Dropbox\MotionDetection_BetaData\Santana-Garcon\garcon_test.avi" --fileD="C:/Users/Ben/Desktop/MotionMeerkatTest/"")
 p = pstats.Stats('C:/MotionMeerkat/profile.txt')
 p.sort_stats('cumulative').print_stats(20)
 
