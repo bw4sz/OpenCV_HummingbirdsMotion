56b338191fd3ef4c12fcf42ee2f77ac6dce8e725
bw4sz <benweinstein2010@gmail.com>
2016-02-26 16:22:43 -0800

    added catch loop

--- MotionMeerkat/Plotting.py
+++ MotionMeerkat/Plotting.py
@@ -19,26 +19,30 @@ def returnplots(array):
 
 def combineplots(minsize,returnframes,mincontour,fname,show):
     
-    plt.figure()
-    #Top Plot
-    ax=plt.subplot(2,1,1)    
-    plt.title("Diagnostics")    
-    minsizeplot(minsize,mincontour)
-    
-    # add legend
-    red_line = mlines.Line2D([], [], color='red', ls='dashed', label='Minimum size set by user')
-    plt.legend(handles=[red_line],bbox_to_anchor=(1, 1),
-               bbox_transform=plt.gcf().transFigure,prop={'size':9})    
-    
-    #Format percentages axis
-    fmt = '%.2f%%' # Format you want the ticks, e.g. '40%'
-    yticks = mtick.FormatStrFormatter(fmt)
-    ax.yaxis.set_major_formatter(yticks)     
-    
-    #Bottom Plot
-    plt.subplot(2,1,2)
-    returnplots(returnframes)
-    plt.tight_layout()    
-    
-    plt.savefig(fname)
+    try:
+        plt.figure(1)
+        #Top Plot
+        ax=plt.subplot(2,1,1)    
+        plt.title("Diagnostics")    
+        minsizeplot(minsize,mincontour)
+        
+        # add legend
+        red_line = mlines.Line2D([], [], color='red', ls='dashed', label='Minimum size set by user')
+        plt.legend(handles=[red_line],bbox_to_anchor=(1, 1),
+                   bbox_transform=plt.gcf().transFigure,prop={'size':9})    
+        
+        #Format percentages axis
+        fmt = '%.2f%%' # Format you want the ticks, e.g. '40%'
+        yticks = mtick.FormatStrFormatter(fmt)
+        ax.yaxis.set_major_formatter(yticks)     
+        
+        #Bottom Plot
+        plt.subplot(2,1,2)
+        returnplots(returnframes)
+        plt.tight_layout()    
+        if show:
+            plt.show()        
+        plt.savefig(fname)
+    except:
+        print("Windows version < 10 may have trouble showing diagnostic plots. Plots were saved to the output folder.")
     
