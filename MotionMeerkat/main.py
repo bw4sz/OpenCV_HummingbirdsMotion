a890a1ecb664cc3517c7799bae8d9a4a4d9ca553
bw4sz <benweinstein2010@gmail.com>
2016-03-17 15:45:31 -0700

    i dind't like where this was going

--- MotionMeerkat/main.py
+++ MotionMeerkat/main.py
@@ -36,7 +36,7 @@ if __name__ == "__main__":
                                 cv2.destroyAllWindows()
                                 traceback.print_exc()
 
-                
+                break
                 
 
             
